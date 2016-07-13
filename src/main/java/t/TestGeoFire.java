69ef1de22eed0cde3736401551049298ea6ee337
maxz <gjnht,yjcnm>
2016-07-14 01:59:16 +0300

    added docs, improved geoFire example

--- src/main/java/t/TestGeoFire.java
+++ src/main/java/t/TestGeoFire.java
@@ -1,9 +1,6 @@
 package t;
 
-import com.firebase.geofire.GeoFire;
-import com.firebase.geofire.GeoLocation;
-import com.firebase.geofire.GeoQuery;
-import com.firebase.geofire.LocationCallback;
+import com.firebase.geofire.*;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.DatabaseError;
@@ -16,20 +13,76 @@ import java.util.concurrent.CountDownLatch;
 
 /**
  * https://github.com/firebase/geofire-java
+ * rules:
+ *      examples:
+ *          https://github.com/firebase/geofire-js/tree/master/examples/securityRules
+ *      doc:
+ *          https://firebase.google.com/docs/database/security/securing-data#predefined_variables
+ *          https://firebase.google.com/docs/database/security/
  */
 public class TestGeoFire {
 
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        final String databaseUrl = "https://testgeo-8865b.firebaseio.com";
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setServiceAccount(new FileInputStream("D:\\work\\test-firebase\\src\\main\\resources\\cred.json"))
-                .setDatabaseUrl("https://test-91459.firebaseio.com")
+                .setServiceAccount(new FileInputStream("D:\\work\\test-firebase\\src\\main\\resources\\cred-geo.json"))
+                .setDatabaseUrl(databaseUrl)
                 .build();
 
         FirebaseApp.initializeApp(options);
 
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReferenceFromUrl("https://test-91459.firebaseio.com");
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReferenceFromUrl(databaseUrl);
         GeoFire geoFire = new GeoFire(ref);
-        geoFire.setLocation("firebase-hq", new GeoLocation(37.7853889, -122.4056973), new GeoFire.CompletionListener() {
+
+        CountDownLatch latch = new CountDownLatch(1);
+        geoFire.getLocation("firebase-hq", new LocationCallback() {
+            @Override
+            public void onLocationResult(String key, GeoLocation location) {
+                latch.countDown();
+                if (location != null) {
+                    System.out.println(String.format("The location for key %s is [%f,%f]", key, location.latitude, location.longitude));
+                } else {
+                    System.out.println(String.format("There is no location for key %s in GeoFire", key));
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                latch.countDown();
+                System.err.println("There was an error getting the GeoFire location: " + databaseError);
+            }
+        });
+        latch.await();
+        GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(37.7853888, -122.4056), 0.6);
+
+        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+            @Override
+            public void onKeyEntered(String s, GeoLocation geoLocation) {
+                System.out.println("onKeyEntered s = " + s + ", loc = " + geoLocation);
+            }
+
+            @Override
+            public void onKeyExited(String s) {
+                System.out.println("onKeyExited s = " + s);
+            }
+
+            @Override
+            public void onKeyMoved(String s, GeoLocation geoLocation) {
+                System.out.println("onKeyMoved s = " + s + ", loc = " + geoLocation);
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+                System.out.println("onGeoQueryReady");
+            }
+
+            @Override
+            public void onGeoQueryError(DatabaseError error) {
+                System.out.println("onGeoQueryError " + error);
+            }
+        });
+
+        geoFire.setLocation("firebase-hq2", new GeoLocation(37.7853889, -122.4056972), new GeoFire.CompletionListener() {
 // FROM example - unexpected argument type 'FirebaseError'
 //            @Override
 //            public void onComplete(String key, FirebaseError error) {
@@ -51,26 +104,6 @@ public class TestGeoFire {
             }
         });
 
-        CountDownLatch latch = new CountDownLatch(1);
-        geoFire.getLocation("firebase-hq", new LocationCallback() {
-            @Override
-            public void onLocationResult(String key, GeoLocation location) {
-                latch.countDown();
-                if (location != null) {
-                    System.out.println(String.format("The location for key %s is [%f,%f]", key, location.latitude, location.longitude));
-                } else {
-                    System.out.println(String.format("There is no location for key %s in GeoFire", key));
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                latch.countDown();
-                System.err.println("There was an error getting the GeoFire location: " + databaseError);
-            }
-        });
-        latch.await();
-        GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(37.7832, -122.4056), 0.6);
     }
 
 }
